b1d191c6269bb129a382c5a90ebfe5862904a37a
Simon Swain <simon@simonswain.com>
2013-09-30 17:01:13 +1000

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    nodeunit: {
+      core: [
+        'test/*.js'
+      ]
+    },
+    jshint: {
+      files: ['Gruntfile.js',
+              'public/js/**/*.js'
+             ],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('default', ['lint']);
+
+};
