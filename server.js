feb9aca3af62baa5ae37a3c71535dedf1c70922f
Simon Swain <simon@simonswain.com>
2013-09-30 17:01:24 +1000

    Server

---
+++ server.js
@@ -0,0 +1,36 @@
+var server;
+var express = require('express');
+var app = express();
+var less = require('less-middleware');
+
+app.port = 3002;
+server = require('http').createServer(app);
+
+app.use(less({
+  force: true,
+  prefix: '/css',
+  src: __dirname + '/public/less',
+  dest: __dirname + '/public/css',
+}));
+
+app.locals.pretty = true;
+app.disable('x-powered-by');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.logger('dev'));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+app.use(express.favicon(__dirname + '/public/images/favicon.ico')); 
+app.use(express.static(__dirname + '/public'));
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(app.router);
+
+require('./routes')(app);
+
+server.listen(app.port);
+
+console.log('Bezerk', 'http://127.0.0.1:' + app.port);
